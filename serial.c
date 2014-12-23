d4b40de0efd4fc28fd46b7d0f9891e5c505d6f3b
Ananya Bahadur <ananya95@gmail.com>
2014-12-23 22:55:52 +0530

    Commented serial.c
    Added some explanation about initialization instructions and data packet format.Need to edit README.md yet on how to run-test-debug-mod serial.c with the python script

--- serial.c
+++ serial.c
@@ -1,12 +1,28 @@
 /*
-continuously writes an int variable
+System Clock 16MHz sourced from external Crystal Oscillator
+  which is on-board.
+Implements USART (transit only) in the following Data Packet Format:
+  <size>		sz	1 byte
+  <data0>		data0	1 byte
+  .		.	.
+  .		.	.
+  <data{sz-1}>	.	.
+Thus totally {sz+1} bytes are required.
+This format could be easily changed to *fixed size* format, if needed.
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+USES:
+  PORTD 0,1 (PORTD0-1){RX and TX}
+  >>>>as USART pins
+----------------------------------------------------------------------
+The program writes 'count' every 2msec (when wFlag is set) and
+decrements it.
 */
 #include<avr/io.h>
 #include<avr/interrupt.h>
 #define BAUD_PRESCALE (F_CPU / (9600 * 16UL)) - 1
 
 volatile uint8_t m,h,d,s,wFlag;
-volatile int ms,;
+volatile int ms;
 
 ISR (TIMER2_COMP_vect){
   ms += 2;
@@ -30,13 +46,13 @@ ISR (TIMER2_COMP_vect){
 }
 
 int main(){
-  int count=1024;
+  int count=1024; // default int size is 2 bytes
   TCNT2 = 0;
   OCR2 = 0xF9; //2 msec (250 states)
   TIMSK |= 1<<OCIE2; //enable compare match with OCR2
-  UCSRB |= (1<<RXEN) | (1<<TXEN);
-  UCSRC |= (1<<URSEL) | (1<<UCSZ0) | (1<<UCSZ1);
-  UBRRH = (BAUD_PRESCALE >> 8);
+  UCSRB |= (1<<RXEN) | (1<<TXEN); //enable USART
+  UCSRC |= (1<<URSEL) | (1<<UCSZ0) | (1<<UCSZ1); // set data packet sz to 8bits
+  UBRRH = (BAUD_PRESCALE >> 8); // set communication bit(baud) rate
   UBRRL = BAUD_PRESCALE;
   TCCR2 |= (1<<WGM21) | (1<<CS22) | (1<<CS20); //set CTC and 128 prescaler
   DDRB = 1<<5;
@@ -44,14 +60,16 @@ int main(){
   sei();
   while (1){
     if (wFlag == 1){
-      while ((UCSRA & (1<<UDRE)) == 0) {;}
+      while ((UCSRA & (1<<UDRE)) == 0) {;} // wait till UDR is ready
       UDR = 2;
+      /* UDR is the Usart Data Register
+      */
       while ((UCSRA & (1<<UDRE)) == 0) {;}
       UDR = count;
       while ((UCSRA & (1<<UDRE)) == 0) {;}
       UDR = count>>8;
       count--;
-      wFlag=0;
+      wFlag=0; // reset flag
     }
   }
   return 0;
