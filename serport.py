5ab83a3f61d05056b1c830e2ea3c056d141c5c11
Ananya Bahadur <ananya95@gmail.com>
2014-12-23 22:37:41 +0530

    Commented serport.py
    Also removed the 10 sec sleep, not required!

--- serport.py
+++ serport.py
@@ -1,17 +1,22 @@
+# This program will represent all data packets as ints, even if your c-code sends chars
+# * make this general, ie accept chars etc?
+
 import serial, argparse, time
+#start a argument parser, we don't need this now but may need it later
 parser = argparse.ArgumentParser(description="Plot data from arduino, specify frequency of data.")
 parser.add_argument("rate", help="Packet interval in milliseconds", type=int)
 args = parser.parse_args()
 ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=None)
-time.sleep(10)
+
+#main reading stuff
 while True:
   data = ser.read()
-  if not data == '':
-    sz = ord(data)
-    info = 0
+  if not data == '':	#enter if-block only if some data has been sent to PC
+    sz = ord(data)	#ord converts char to int
+    info = 0		#actual data sent in packet (currently only working with ints)
     for i in range(0,sz):
       data = ser.read()
-      info += ord(data)*(256**i)
+      info += ord(data)*(256**i)	#some trivial math
     if info>60000:
       print '*', info
     else:
