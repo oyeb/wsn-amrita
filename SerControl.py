1554fb9644fda06a8d5d680d98d19e4dceb461ea
Ananya Bahadur <ananya95@gmail.com>
2015-01-02 00:14:05 +0530

    Upgraded tk0.py to SerControl.py
    GUI can now open a port, but that block (freezes) the GUI because of the forever-loop to poll the Port-Data.

--- SerControl.py
+++ SerControl.py
@@ -9,13 +9,27 @@
  This program is solely to test and debug programs, it may never be 
  maintained for future or generalised. Not much time should be devoted to
  this.
+ 
+ CURRENTLY
+   Only listens on port, does not send any data.
+   GUI freezes upon pressing 'Connect' as a busy-while loop vlocks everything
+   Must handover that task to a thread...
 '''
 import Tkinter as tk
+import Queue, serial, threading
+# Next line, is the serial port being used by the device
+# It should be the same one as you see @ bottom right corner of ARDUINO IDE
+# afer connecting the device
+PORT = '/dev/ttyUSB0'
 
 class Application(tk.Frame):
   def __init__(self, master=None):
     tk.Frame.__init__(self, master)
     self.grid()
+    # the serial port
+    self.ser = serial.Serial()
+    # a status flag, denotes whether we have opened a serial port
+    self.serUP = False
     self.createWidgets()
   
   def createWidgets(self):
@@ -44,13 +58,38 @@ class Application(tk.Frame):
     self.quitButton.grid(row=1, column=0, sticky=tk.E)
   
   def init_ser(self):
-    print 'Initialising serial port!'
+    try:
+      self.ser.port = PORT
+      self.ser.timeout = None
+      self.ser.open()
+      self.serUP = True
+      print '*_* Opened', PORT, 'listening for messages!'
+      while True:
+        data = self.ser.read()
+        if not data == '':	#enter if-block only if some data has been sent to PC
+          sz = ord(data)	#ord converts char to int
+          info = 0		#actual data sent in packet (currently only working with ints)
+          for i in range(0,sz):
+            data = self.ser.read()
+            info += ord(data)*(256**i)	#some trivial math
+          if info>60000:
+            print '* ', info
+          else:
+            print info
+    except:
+      print '*_* Error in setting up port @ ', PORT
 
   def end_ser(self):
-    print 'Closed port!!'
+    if self.serUP:
+      self.ser.close()
+      self.serUP = False
   
   def __parse(self, event):
-    print 'Parse user command and sending to serial'
+    cmd = self.cmd.get()
+    if not cmd=='$ ':
+      # log the command onto the pink pane
+      self.oflux.insert('1.0', cmd+'\n')
+    self.cmd.delete('2', tk.END)
 
 app = Application()
 app.master.title('T.A.L.K')
